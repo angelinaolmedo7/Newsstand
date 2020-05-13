51fdd55a452aad7b435414e41eb0fc242c12a465
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-05-12 17:06:47 -0700

    Initial Commit

---
+++ Newsstand/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsstand
+//
+//  Created by Angelina Olmedo on 5/12/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
